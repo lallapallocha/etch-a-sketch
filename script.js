a14d0644876aa08241b9cb067fce3b3b91d9c411
Federico <lallapallochax@gmail.com>
2024-10-26 20:52:32 +0200

    FEAT: rgb trail on mouse over

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-const gridContainer = document.querySelector("#gridContainer");
+let gridContainer = document.querySelector("#gridContainer");
+const buttonResetGridSize = document.querySelector("#resetGridSize");
+const bodyDocument = document.querySelector("body");
 
 const createGrid = (size) => {
-  for (let i = 0; i <= size; i++) {
-    const newRow = document.createElement("div");
-    newRow.id = "gridColumn";
-    for (let j = 0; j <= size; j++) {
-      const newDiv = document.createElement("div");
-      newDiv.id = "gridElement";
-      newDiv.textContent = `${j}`;
-      newRow.appendChild(newDiv);
-    }
-    gridContainer.appendChild(newRow);
+  const proportionalSize = 100 / size; //100 is the whole container
+  for (let i = 0; i < size * size; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.id = "gridElement";
+
+    newDiv.style.width = `${proportionalSize}%`; //it takes a % of the container
+    newDiv.style.height = `${proportionalSize}%`;
+
+    gridContainer.appendChild(newDiv);
   }
 };
 
+const randomColor = () => {
+  return Math.floor(Math.random() * 16777215).toString(16);
+};
+
+const addGridEventListener = (gridContainer) => {
+  gridContainer.addEventListener("mouseover", (event) => {
+    const target = event.target;
+    console.log(target);
+    if (target.id === "gridElement") {
+      target.style.backgroundColor = `#${randomColor()}`;
+    }
+  });
+};
+
+buttonResetGridSize.addEventListener("click", (event) => {
+  let newSize = window.prompt("New grid size:");
+  gridContainer.remove();
+
+  gridContainer = document.createElement("div");
+  gridContainer.id = "gridContainer";
+
+  bodyDocument.appendChild(gridContainer);
+
+  createGrid(newSize);
+  addGridEventListener(gridContainer);
+});
+
 createGrid(16);
+addGridEventListener(gridContainer);
